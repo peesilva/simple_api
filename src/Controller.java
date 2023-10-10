036a49c25abfed80b79c6cf5889af59afcde0556
Pedro Silva <arbeit.pedro.silva@gmail.com>
2023-10-09 22:07:46 -0300

    training API product

---
+++ src/Controller.java
@@ -0,0 +1,21 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class Controller {
+    @Autowired
+    private ProductRepository productRepository;
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    @PostMapping
+    public Product createProduct(@RequestBody Product product) {
+        return productRepository.save(product);
+    }
+}
